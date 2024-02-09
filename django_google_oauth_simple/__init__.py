c0d7a0fb3d350615506f261f2e804055f9d08f89
Marcelo Gumercino Costa <marcelo.costa@Luizalabs.com>
2024-02-08 23:25:46 -0300

    add verificação do email

--- django_google_oauth_simple/__init__.py
+++ django_google_oauth_simple/__init__.py
@@ -3,6 +3,18 @@ from django.conf import settings
 default_settings = {
     'LOGIN_URL': 'login',
     'LOGOUT_URL': 'logout',
+    'SOCIAL_AUTH_PIPELINE': (
+        'social_core.pipeline.social_auth.social_details',
+        'social_core.pipeline.social_auth.social_uid',
+        'social_core.pipeline.social_auth.auth_allowed',
+        'social_core.pipeline.social_auth.social_user',
+        'social_core.pipeline.user.get_username',
+        'social_core.pipeline.social_auth.associate_by_email',
+        'social_core.pipeline.user.create_user',
+        'social_core.pipeline.social_auth.associate_user',
+        'social_core.pipeline.social_auth.load_extra_data',
+        'social_core.pipeline.user.user_details',
+    ),
 }
 
 additional_settings = {
